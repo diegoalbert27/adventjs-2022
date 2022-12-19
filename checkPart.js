da7c90af54d6d413f64f689525923f0d8514d51c
Diego Hinagas <hinagasrodriguez@gmail.com>
2022-12-18 22:17:17 -0400

    Added description the exercises

--- checkPart.js
+++ checkPart.js
@@ -1,3 +1,22 @@
+/** 
+ * Se han estropeado algunos trineos eléctricos y los elfos están buscando piezas de repuesto para
+ * arreglarlos, pero no tienen claro si las piezas que tienen sirven.
+ * 
+ * Las piezas de repuesto son cadenas de texto y el mecánico Elfon Masc ha dicho que una pieza de repuesto
+ * es válida si la pieza puede ser un palíndromo después de eliminar, como máximo, un carácter.
+ * Un palíndromo es una palabra o frase que se lee igual de izquierda a derecha que de derecha a izquierda.
+ * Nuestra función debe devolver un booleano que indique si la pieza de repuesto es válida o no con esa regla:
+ * 
+ * checkPart("uwu") // true
+ * // "uwu" es un palíndromo sin eliminar ningún carácter
+ * checkPart("miidim") // true
+ * // "miidim" puede ser un palíndromo después de eliminar la primera "i"
+ * // ya que "midim" es un palíndromo
+ * 
+ * checkPart("midu") // false
+ * // "midu" no puede ser un palíndromo después de eliminar un carácter
+*/
+
 function checkPart(part) {
   const charts = part.split('')
   if (charts.reverse().join('') === part) {
@@ -19,9 +38,9 @@ function checkPart(part) {
 console.log(checkPart("uwu")) // true
 // "uwu" es un palíndromo sin eliminar ningún carácter
 
-// checkPart("miidim") // true
+checkPart("miidim") // true
 // "miidim" puede ser un palíndromo después de eliminar la primera "i"
 // ya que "midim" es un palíndromo
 
-// checkPart("midu") // false
+checkPart("midu") // false
 // "midu" no puede ser un palíndromo después de eliminar un carácter
